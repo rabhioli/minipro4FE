18fc353b5e08501b5114d784a74f68e67855ca66
rabhi alqadi <rabhialqadi22@gmail.com>
2024-04-05 11:43:25 -0700

    items

--- src/App.js
+++ src/App.js
@@ -44,7 +44,41 @@ class App extends Component {
       </div>
     );
   };
-
+// Main variable to render items on the screen
+renderItems = () => {
+  const { viewCompleted } = this.state;
+  const newItems = this.state.taskList.filter(
+    item => item.completed === viewCompleted
+  );
+  return newItems.map(item => (
+    <li
+      key={item.id}
+      className="list-group-item d-flex justify-content-between align-items-center"
+    >
+      <span
+        className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+        title={item.description}
+      >
+        {item.title}
+      </span>
+      <span>
+        <button
+          onClick={() => this.editItem(item)}
+          className="btn btn-secondary mr-2"
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => this.handleDelete(item)}
+          className="btn btn-danger"
+        >
+          Delete
+        </button>
+      </span>
+    </li>
+  ));
+};
 
 
 }
